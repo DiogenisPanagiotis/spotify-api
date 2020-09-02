d97cf3903a39ac69d287412f78c5a9c4110fa167
diogenispanagiotis <diogenis.panagiotis@gmail.com>
2020-09-02 11:03:50 -0400

    Updates

---
+++ precache-manifest.3db054bf2e66746bb6aa5c7323a16ad1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dad80cc14c9c98ced9f82bd9716dac6b",
+    "url": "/spotify/index.html"
+  },
+  {
+    "revision": "43adc022ca0cea4a1f31",
+    "url": "/spotify/static/css/main.17c0623c.chunk.css"
+  },
+  {
+    "revision": "ed3fa15945157ce18bdd",
+    "url": "/spotify/static/js/2.6d4bc5bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/spotify/static/js/2.6d4bc5bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43adc022ca0cea4a1f31",
+    "url": "/spotify/static/js/main.0fcf24c5.chunk.js"
+  },
+  {
+    "revision": "24ac1e8150c240b40e14",
+    "url": "/spotify/static/js/runtime-main.ba0f994b.js"
+  }
+]);
